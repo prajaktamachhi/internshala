ea8744473648e799c1c4d1b5247f3ef3946ca12c
prajaktamachhi <69513645+prajaktamachhi@users.noreply.github.com>
2021-07-14 13:59:35 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+<footer>     
+	<div class="container">         
+	<center>             
+	<p>Copyright &copy; Lifestyle Store. All Rights Reserved  |  Contact Us: +91 900000000</p>          
+	</center>     
+	</div> 
+</footer> 
+ 
